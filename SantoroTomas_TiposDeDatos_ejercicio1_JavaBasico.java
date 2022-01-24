8c466f8caa83c7731ef96af32720388a1ebe919b
tomassantoro2 <78187091+tomassantoro2@users.noreply.github.com>
2022-01-23 23:08:29 -0300

    Add files via upload

---
+++ SantoroTomas_TiposDeDatos_ejercicio1_JavaBasico.java
@@ -0,0 +1,65 @@
+package ProyectoS1;
+
+
+
+
+public class ProyectoS1 {
+
+
+    public static void main(String[] args) {
+        // Enteros
+        byte entero1= 1;
+        short entero2 = 2;
+        int entero3 = 3;
+        long entero4 = 4l;
+
+        // decimales
+        float flotante1 = 4.5f;
+        double flotante2= 10.99d;
+
+        // booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //caracter
+        char caracter1 = 'a';
+
+        // cadena de texto
+        String texto = "txt";
+
+        // tipos envoltorio
+        Integer numero1 = 6;
+        Long numero2 = 6L;
+
+
+
+        //Impresion!
+
+
+
+        // Enteros
+        System.out.println(entero1);
+        System.out.println(entero2);
+        System.out.println(entero3);
+        System.out.println(entero4);
+
+        //Decimales
+        System.out.println(flotante1);
+        System.out.println(flotante2);
+
+        //booleanos
+        System.out.println(verdadero);
+        System.out.println(falso);
+
+        //caracter
+        System.out.println(caracter1);
+
+        //cadena de texto
+        System.out.println(texto);
+
+        //tipos envoltorio
+        System.out.println(numero1);
+        System.out.println(numero2);
+    }
+
+    }
