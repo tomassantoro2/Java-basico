d78afbce547f674cbdb4f5d26fb1755a54b9a3bd
tomassantoro2 <78187091+tomassantoro2@users.noreply.github.com>
2022-01-27 20:18:39 -0300

    Add files via upload

---
+++ ProyectoS2.java
@@ -0,0 +1,16 @@
+package ProyectoS1;
+
+public class ProyectoS2 {
+    public static void main(String[] args) {
+        // Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.
+
+
+        CalculadoraIva(100);
+    }
+        // EL iva en mi pais es del 21%
+        static void CalculadoraIva(int precio) { System.out.println("Su precio final es " + precio * 1.21); }
+
+
+
+
+}
